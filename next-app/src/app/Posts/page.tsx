63070547d279000cb0f54426ab192e70b25d7ed5
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-02-12 18:56:35 +0000

    init next.js

---
+++ next-app/src/app/Posts/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Posts(){
+    return (
+        <section>
+            <div>
+            <h1>All Posts</h1>
+        </div>
+        <div>
+            <p>Post 1</p>
+            <p>Post 2</p>
+        </div>
+        </section>
+    );
+}
